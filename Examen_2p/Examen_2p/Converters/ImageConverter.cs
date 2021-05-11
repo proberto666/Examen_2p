261db67b06bc24b9ccd98273be3cb14d65b9b5e4
Víctor Manuel Nevárez Herrera <victor.m.nevarez.h@gmail.com>
2021-05-11 12:41:03 -0500

    ImageConverter en Converters / ImageService en Services

--- Examen_2p/Examen_2p/Converters/ImageConverter.cs
+++ Examen_2p/Examen_2p/Converters/ImageConverter.cs
@@ -1,10 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Globalization;
+using Xamarin.Forms;
+using Examen_2p.Services;
 
 namespace Examen_2p.Converters
 {
-    class ImageConverter
+    public class ImageConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || string.IsNullOrEmpty(value.ToString())) return null;
+
+
+            return new ImageService().ConvertImageFromBase64ToImageSource(value.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
